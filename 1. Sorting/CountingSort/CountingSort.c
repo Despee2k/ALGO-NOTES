3ea47832c0ce0f49167863df99c906d623d3c757
Dempsey <dagantareynat@gmail.com>
2025-03-10 15:16:50 +0800

    refactor: change code structure to follow new format and add comments for clarity

--- 1. Sorting/CountingSort/CountingSort.c
+++ 1. Sorting/CountingSort/CountingSort.c
@@ -1,48 +1,46 @@
+#include "../../include/utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 10
-
-void printer(int arr[]){
-    for(int i = 0; i < MAX; i++){
-        printf("[%d]", arr[i]);
+void countingSort(int arr[], int size){
+    // look for largest number in the array to use as index
+    int largest = 0;
+    for(int i = 0; i < size; i++){
+        if(largest < arr[i]) largest = arr[i];
     }
-    printf("\n");
-}
+    printf("%d\n", largest);
+    
+    int *newArr = calloc((largest + 1), sizeof(int));
 
-void countingSort(int arr[]){
-    int maxVal = 0;
-
-    for(int i = 0; i < MAX; i++){
-        if(maxVal < arr[i]) maxVal = arr[i];
-    }
-
-    int newMax = maxVal + 1;
-
-    int *newArr = calloc(newMax, sizeof(int));
-    for(int i = 0; i < MAX; i++){
+    // increment each index based on how many times it appears in the original array
+    for(int i = 0; i < size; i++){
         newArr[arr[i]]++;
     }
 
-    int maxAgain = MAX + 1;
-    for(int i = 1; i < maxAgain; i++){
+    // add previous counts
+    int sizePlusOne = size + 1;
+    for(int i = 1; i < sizePlusOne; i++){
         newArr[i] = newArr[i] + newArr[i-1];
     }
 
-    int outputArr[MAX];
-    for(int i = MAX - 1; i >= 0; i--){
+    // corresponding values represent the places in the count array
+    int *outputArr = malloc(size * sizeof(int));
+    for(int i = size - 1; i >= 0; i--){
         outputArr[--(newArr[arr[i]])] = arr[i];
     }
 
-    memcpy(arr, outputArr, sizeof(outputArr));
+    memcpy(arr, outputArr, size * sizeof(int));
     free(newArr);
+    free(outputArr);
 }
 
 int main(void){
-    int arr[MAX] = {8, 9, 1, 7, 3, 10, 5, 4, 2, 6};
-    countingSort(arr);
-    printer(arr);
+    int arr[] = {8, 9, 1, 7, 3, 10, 5, 4, 2, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    countingSort(arr, size);
+    printer(arr, size);
 
     return 0;
 }
